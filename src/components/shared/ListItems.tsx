0e967500b74404972c5f52b4ea434aee54b440af
LautaroLD <duranlautarogabriel@gmail.com>
2024-05-12 21:35:52 -0300

    drop(-): Remove unused ListItems component from shared components directory

--- src/components/shared/ListItems.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const ListItems = async ({ url }) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-  return <div>ListItems</div>;
-};
